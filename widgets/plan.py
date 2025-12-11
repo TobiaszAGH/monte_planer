b6457dcd3e168dbd719ae05fca016cff0246f6ff
Tobiasz <tobiasz@student.agh.edu.pl>
2025-12-11 11:14:31 +0100

    drawing rectangles on plan

--- widgets/plan.py
+++ widgets/plan.py
@@ -1,7 +1,7 @@
 
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QComboBox, QHBoxLayout, QDialog, QDialogButtonBox, \
       QPushButton, QLabel, QDialogButtonBox, QMessageBox, QInputDialog, QGridLayout, QCheckBox, QSizePolicy, \
-      QGraphicsScene, QGraphicsView, QSpacerItem
+      QGraphicsScene, QGraphicsView, QSpacerItem, QToolBar, QToolTip, QGraphicsItem, QGraphicsRectItem
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPen, QBrush
 
@@ -15,74 +15,134 @@ class MyView(QGraphicsView):
         self.setScene(QGraphicsScene())
         self.db = parent.db
         self.class_names = []
+        self.mode = ''
+        self.block_start = 0
+        self.new_block = False
+        self.setMouseTracking(True)
+        self.top_bar_h = 75
+        self.left_bar_w = 50
+        self.update_size_params()
 
     def set_class_names(self, class_names):
         self.class_names = class_names
 
+    def update_size_params(self):
+        self.scene_width = self.geometry().width()-10
+        self.scene_height = self.geometry().height()-10
+        self.hour_h = (self.scene_height-self.top_bar_h)/8
+        self.five_min_h = self.hour_h/12
+        self.day_w = (self.scene_width-self.left_bar_w)/5
+        l = len(self.class_names)
+        if l>0:
+            self.block_w = self.day_w/l
+
     def resizeEvent(self, event):
         QGraphicsView.resizeEvent(self, event)
+
+        self.update_size_params()
         self.draw_frame()
+
+    def set_mode_new(self):
+        self.mode = 'new'
+    
+    def mousePressEvent(self, event):
+        if self.mode == 'new':
+            l = len(self.class_names)
+            if l == 0:
+                return False
+            self.block_start = self.how_many_5_min_blocks(event)
+            self.new_block_top = (event.y() - self.top_bar_h) // self.five_min_h * self.five_min_h + self.top_bar_h
+            self.new_block_left = (event.x() - self.left_bar_w) // self.block_w * self.block_w + self.left_bar_w
+            self.new_block = self.scene().addRect(self.new_block_left, self.new_block_top, self.block_w, self.five_min_h)
+
+
+    def mouseReleaseEvent(self, event):
+        self.block_start = 0
+        self.new_block = False
+        # if self.mode == 'new':
+
+    def display_hour(self, mins):
+        hs = int(mins//12+8)
+        mins = int(mins%12)*5
+        return f'{hs}:{mins:02d}' 
+    
+    def how_many_5_min_blocks(self, event):
+        y = event.y() - self.top_bar_h
+        mins = y//self.five_min_h
+        return mins
+        
+
+    def mouseMoveEvent(self, event=0):
+        if self.mode == 'new':
+            now = self.how_many_5_min_blocks(event)
+            times = [now, self.block_start] if self.block_start else [now]
+            times.sort()
+            msg = '-'.join([self.display_hour(t) for t in times])
+            if self.block_start:
+                msg += f' ({int(abs(now-self.block_start)*5)})'
+
+            QToolTip.showText(event.globalPos(), msg)
+            if self.new_block:
+                new_block_bottom = (event.y() - self.top_bar_h) // self.five_min_h * self.five_min_h + self.top_bar_h
+                height = abs(new_block_bottom - self.new_block_top)
+                y = min(new_block_bottom, self.new_block_top)
+                self.new_block: QGraphicsRectItem
+                
+                self.new_block.setRect(self.new_block_left, y, self.block_w, height)
+                # print(self.new_block)
+        pass
 
     def draw_frame(self):
         scene = self.scene()
         scene.clear()
-        width = self.geometry().width()-10
-        height = self.geometry().height()-10
-        top_bar_h = 75
-        left_bar_w = 50
-
-
-        scene.setSceneRect(0,0, width, height)
+        scene.setSceneRect(0,0, self.scene_width, self.scene_height)
 
         wide_pen = QPen()
         wide_pen.setWidth(2)
-        line = scene.addLine(0, top_bar_h, width, top_bar_h)
+        line = scene.addLine(0, self.top_bar_h, self.scene_width, self.top_bar_h)
         line.setPen(wide_pen)
-        line = scene.addLine(0, 0, width, 0)
+        line = scene.addLine(0, 0, self.scene_width, 0)
         line.setPen(wide_pen)
-        line = scene.addLine(0, 0, 0, height)
+        line = scene.addLine(0, 0, 0, self.scene_height)
         line.setPen(wide_pen)
-        line = scene.addLine(0, height, width, height)
+        line = scene.addLine(0, self.scene_height, self.scene_width, self.scene_height)
         line.setPen(wide_pen)
-        line = scene.addLine(left_bar_w, 0, left_bar_w, height)
+        line = scene.addLine(self.left_bar_w, 0, self.left_bar_w, self.scene_height)
         line.setPen(wide_pen)
 
-        hour_h = (height-top_bar_h)/8
-        five_min_h = hour_h/12
         for hour in range(8,16):
-            pos = top_bar_h+(hour - 7)*hour_h
+            pos = self.top_bar_h+(hour - 7)*self.hour_h
             text = scene.addSimpleText(f'{hour}-{hour+1}')
 
             # center text
-            text_x = (left_bar_w-text.boundingRect().width())/2
-            text_y = pos-(hour_h+text.boundingRect().height())/2
+            text_x = (self.left_bar_w-text.boundingRect().width())/2
+            text_y = pos-(self.hour_h+text.boundingRect().height())/2
             text.setPos(text_x, text_y)
 
-            scene.addLine(0, pos, left_bar_w, pos)
+            scene.addLine(0, pos, self.left_bar_w, pos)
 
         l = len(self.class_names)
-        day_w = (width-left_bar_w)/5
         days = 'Poniedziałek Wtorek Środa Czwartek Piątek'.split()
         for day in range(5):
-            pos = day_w*(day+1)+left_bar_w
-            line = scene.addLine(pos, 0, pos, height)
+            pos = self.day_w*(day+1)+self.left_bar_w
+            line = scene.addLine(pos, 0, pos, self.scene_height)
             line.setPen(wide_pen)
             
             text = scene.addSimpleText(days[day])
-            text_x = pos - (day_w + text.boundingRect().width())/2
-            text_y = (top_bar_h/2 - text.boundingRect().height())/2
+            text_x = pos - (self.day_w + text.boundingRect().width())/2
+            text_y = (self.top_bar_h/2 - text.boundingRect().height())/2
             text.setPos(text_x, text_y)
         if l>0:
-            scene.addLine(left_bar_w, top_bar_h/2, width, top_bar_h/2)
-            block_w = day_w/l
+            scene.addLine(self.left_bar_w, self.top_bar_h/2, self.scene_width, self.top_bar_h/2)
+            self.block_w = self.day_w/l
             for i in range(5):
                 for n, class_name in enumerate(self.class_names):
-                    pos = block_w*(n+i*l+1)+left_bar_w
+                    pos = self.block_w*(n+i*l+1)+self.left_bar_w
                     text = scene.addSimpleText(class_name)
-                    text_x = pos - (text.boundingRect().width()+block_w)/2
-                    text_y = top_bar_h/2 + (top_bar_h/2 - text.boundingRect().height())/2
+                    text_x = pos - (text.boundingRect().width()+self.block_w)/2
+                    text_y = self.top_bar_h/2 + (self.top_bar_h/2 - text.boundingRect().height())/2
                     text.setPos(text_x, text_y)
-                    scene.addLine(pos, top_bar_h/2, pos, height)
+                    scene.addLine(pos, self.top_bar_h/2, pos, self.scene_height)
 
             # print(self.data['blocks'].keys())
             # for n, class_name in enumerate(self.class_names):
@@ -116,9 +176,30 @@ class PlanWidget(QWidget):
         self.class_filter.layout().addStretch()
         self.load_classes()
         layout.addWidget(self.class_filter)
+
+
+        toolbar = QWidget()
+        layout.addWidget(toolbar)
+        toolbar.setLayout(QHBoxLayout())
+        tool_add_block = QPushButton("N", toolbar)
+        toolbar.layout().addWidget(tool_add_block)
+        tool_add_block.setAutoExclusive(True)
+        tool_add_block.setCheckable(True)
+        tool_add_block.clicked.connect(self.set_mode_new)
+        tool_move_block = QPushButton("M", toolbar)
+        toolbar.layout().addWidget(tool_move_block)
+        tool_move_block.setAutoExclusive(True)
+        tool_move_block.setCheckable(True)
+        
+
         self.view = MyView(self)
         layout.addWidget(self.view)
         self.load_data()
+
+        
+        
+    def set_mode_new(self):
+        self.view.set_mode_new()
 
     
 
