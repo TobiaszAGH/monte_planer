2f624bac59830cc34a3007840c154d4db4d27df3
Tobiasz Salik <tobiasz@student.agh.edu.pl>
2025-12-17 15:39:09 +0100

    updated functions.py

--- functions.py
+++ functions.py
@@ -1,2 +1,9 @@
 def snap_position(pos:float, unit:float, ofset=0.0, up=False):
     return (pos-ofset)//unit*unit+ofset+unit*up
+
+
+
+def display_hour(mins):
+    hs = int(mins//12+8)
+    mins = int(mins%12)*5
+    return f'{hs}:{mins:02d}' 
