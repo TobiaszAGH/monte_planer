83302e494ae0b05af2f5e5b7c580f5367da129ec
Tobiasz Salik <tobiasz@student.agh.edu.pl>
2025-12-23 12:15:51 +0100

    plan view scaling

--- tabs/plan/plan.py
+++ tabs/plan/plan.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QSlider, QLabel
+from PyQt5.QtCore import Qt
 from data import Data
 from .mode_btn import ModeBtn
 from .plan_view import MyView
@@ -29,6 +30,18 @@ class PlanWidget(QWidget):
         toolbar.layout().addWidget(tool_move_block)
         self.tool_add_custom = ModeBtn("Nowy blok", self.set_mode_new_custom ,toolbar)
         toolbar.layout().addWidget(self.tool_add_custom)
+        self.scale_slider = QSlider(Qt.Horizontal, self)
+        self.scale_slider.setMaximumWidth(150)
+        self.scale_slider.setMinimum(100)
+        self.scale_slider.setMaximum(300)
+        self.scale_slider.setSingleStep(10)
+        self.scale_slider.setPageStep(50)
+        self.scale_slider.setTickPosition(QSlider.TicksAbove | QSlider.TicksBelow)
+        self.scale_slider.setTickInterval(50)
+        self.scale_slider.valueChanged.connect(self.update_scale)
+        toolbar.layout().addWidget(self.scale_slider)
+        self.scale_label = QLabel('100%', self.scale_slider)
+        toolbar.layout().addWidget(self.scale_label)
         toolbar.layout().addStretch()
         
 
@@ -36,7 +49,10 @@ class PlanWidget(QWidget):
         layout.addWidget(self.view)
         self.load_data()
 
-        
+    def update_scale(self, value):
+        self.scale_label.setText(f'{value}%')
+        self.view.resetTransform()
+        self.view.scale(value/100, value/100)
         
     def set_mode_new(self, checked):
         if checked:
